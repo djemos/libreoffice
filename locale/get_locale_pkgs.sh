e4de4d381f2861d7d476425bda63892b5f4fa4c4
djemos <dijemos@gmail.com>
2015-09-03 18:36:36 +0300

    update builds

--- locale/get_locale_pkgs.sh
+++ locale/get_locale_pkgs.sh
@@ -6,7 +6,8 @@ eval $(grep '^\s*VERSION=' libreoffice-l10n.SlackBuild)
 arch=${arch:-x86_64}
 HWarch=$([ $arch = x86_64 ] && echo "x86_64" || echo "x86")
 
-URL=http://download.documentfoundation.org/libreoffice/stable/$VERSION/rpm/$HWarch/
+#URL=http://download.documentfoundation.org/libreoffice/stable/$VERSION/rpm/$HWarch/
+URL=http://mirror.catn.com/pub/tdf/libreoffice/stable/$VERSION/rpm/$HWarch/
 
 pkgdir=langpacks
 
